061fee141ad50008a818b61687e3a3fd38debe43
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-04-25 11:19:51 +0300

    Add round function

--- script.js
+++ script.js
@@ -11,4 +11,24 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function round(playerSelection,computerSelection){
+    let playerSelectionInsesitive = playerSelection.toLowerCase();
+    if(playerSelectionInsesitive === computerSelection){
+        return `Its a tie, You both played ${playerSelectionInsesitive}`;
+    }else if(playerSelectionInsesitive === "rock" && computerSelection === "paper"){
+        return `You lose! Paper beats ${playerSelectionInsesitive}`;
+    }else if(playerSelectionInsesitive === "rock" && computerSelection === "scissors"){
+        return `You Win! Rock beats ${computerSelection}`;
+    }else if(playerSelectionInsesitive === "paper" && computerSelection === "scissors"){
+        return `You lose! Scissors beats ${playerSelectionInsesitive}`;
+    }else if(playerSelectionInsesitive === "paper" && computerSelection === "rock"){
+        return `You Win! Paper beats ${computerSelection}`;
+    }else if(playerSelectionInsesitive === "scissors" && computerSelection === "rock"){
+        return `You Lose! Rock beats ${playerSelectionInsesitive}`;
+    }else if(playerSelectionInsesitive === "scissors" && computerSelection === "paper"){
+        return `You Win! Scissors beats ${computerSelection}`;
+    }
+}
+
+
+console.log(round(getComputerChoice(),getComputerChoice()));
